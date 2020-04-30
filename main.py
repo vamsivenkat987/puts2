c769686358aa82f3d3d61da0dae9903a8ad167b4
vamsivenkat987 <vamsivenkat1998@gmail.com>
2020-04-30 21:50:16 +0200

    first commit

--- main.py
+++ main.py
@@ -1,83 +1,34 @@
 from flask import Flask, request
 from fractions import Fraction
 from decimal import Decimal
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return 'write values of A and B in the format of url/operation? A=arg1 & B=arg2'
-	
+    return 'gives the addition between arg1 and arg2'
+
+
 @app.route('/add')
 def addition():
-    try:
-        arg1 = request.args.get('A',default = 0, type = Fraction)
-    except ZeroDivisionError:
-        arg1 = 'None'
-    try:
-        arg2 = request.args.get('B',default = 0, type = Fraction)
-    except ZeroDivisionError:
-        arg2 = 'None'
-    if arg1=='None' or arg2 == 'None':
-        return'Error:check the args given above'
+    arg1=request.args.get('A',default = 0, type = Fraction)
+    arg2=request.args.get('B',default = 0, type = Fraction)
+    X= Fraction(arg1)
+    Y= Fraction(arg2)
+    Z=X+Y
+   
+    result = str(Z).split('/')
+    if len(result) == 2:
+        first_arg = int(result[0])/int(result[1])
+        second_arg = str(first_arg).split('.')
+        if second_arg[1] == '0':
+            return '$ \n'% second_arg[0]
+        else:
+            return '$ \n' % first_arg
     else:
-        X= Fraction(arg1)
-        Y = Fraction(arg2)
-        Z = X+Y
-        return str(round(float(Z),4))
+        result1=str(Z).split('.')
+        return '$ \n'% result1[0]
 
-@app.route('/sub')
-def substraction():
-    try:
-        arg1 = request.args.get('A' , default = 0, type = Fraction)
-    except ZeroDivisionError:
-        arg1 = 'None'
-    try:
-        arg2 = request.args.get('B',default =0, type = Fraction)
-    except ZeroDivisionError:
-        arg2 = 'None'
-    if arg1 == 'None' or arg2 == 'None':
-        return'Error:check the args given above'
-    else:
-        X= Fraction(arg1)
-        Y = Fraction(arg2)
-        Z = X-Y
-        return str(round(float(Z),4))
-        
-@app.route('/mul')
-def multiplication():
-    try:
-        arg1 = request.args.get('A' , default=0, type = Fraction)
-    except ZeroDivisionError:
-        arg1 = 'None'
-    try:
-        arg2 = request.args.get('B' , default=0, type = Fraction)
-    except ZeroDivisionError:
-        arg2 = 'None'
-    if arg1 == 'None' or arg2 == 'None':
-        return 'Error:check the args given above'
-    else:
-        X= Fraction(arg1)
-        Y = Fraction(arg2)
-        Z = X*Y
-        return str(round(float(Z),4))
-       
-@app.route('/div')
-def division():
-    try:
-        arg1 = request.args.get('A' , default=0, type = Fraction)
-    except ZeroDivisionError:
-        arg1 = 'None'
-    try:
-        arg2 = request.args.get('B' , default=0, type = Fraction)
-    except ZeroDivisionError:
-        arg2 = 'None'
-    if arg1 == 'None' or arg2 == 'None':
-        return 'Error:check the args given above'
-    else:
-        X= Fraction(arg1)
-        Y = Fraction(arg2)
-        Z = X/Y
-        return str(round(float(Z),4))
-    
-    
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()

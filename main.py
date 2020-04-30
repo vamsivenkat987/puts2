e0441935d5b3e0913560edb7d391dec9a98359f0
vamsivenkat987 <vamsivenkat1998@gmail.com>
2020-04-30 21:59:38 +0200

    first commit

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'gives the addition between arg1 and arg2'
+    return 'gives the subtraction between arg1 and arg2'
 
 
-@app.route('/add')
-def addition():
+@app.route('/sub')
+def subtraction():
     arg1=request.args.get('A',default = 0, type = Fraction)
     arg2=request.args.get('B',default = 0, type = Fraction)
     X= Fraction(arg1)
     Y= Fraction(arg2)
-    Z=X+Y
+    Z=X-Y
    
     result = str(Z).split('/')
     if len(result) == 2:
